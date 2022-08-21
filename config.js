fe8493485b1685aa5ec120138cc3eeadf6b9f6b5
skuderos <jordoe@live.nl>
2022-08-21 18:23:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var config = {
   PORT: process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  IPADDRESS: process.env.OPENSHIFT_NODEJS_IP || '77.169.232.101',
+  IPADDRESS: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
   TILES_DIR: process.env.OPENSHIFT_DATA_DIR || path.join(__dirname, '/data'),
   MAP_DIR: path.join(__dirname, '/static/map'),
 }
